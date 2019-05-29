812b1d64970b2d24a6ac13401d6821df4fe90004
l1z1b <echoi@alumni.ubc.ca>
2019-05-29 14:24:12 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Link } from 'react-router-dom';
+import Main from './components/main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="demo-big-content">
+          <Layout>
+              <Header className="header-color" title="Title" scroll>
+                  <Navigation>
+                      <Link to="/aboutme"> About Me</Link>
+                  </Navigation>
+              </Header>
+              <Drawer title="Title">
+                  <Navigation>
+                      <Link to="/aboutme"> About Me</Link>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                  <div className="page-content" />
+                  <Main></Main>
+              </Content>
+          </Layout>
+      </div>
   );
 }
 
