6dee62c68873f85ee4ff7295590a3d04cf4341ec
l1z1b <echoi@alumni.ubc.ca>
2019-06-10 01:27:47 -0700

    Add server side dependencies

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React from 'react';
-import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import {Link} from 'react-router-dom';
 import Main from './components/main';
 
-function App() {
-  return (
-      <div className="demo-big-content"  >
-          <Layout >
-              <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Functionary</Link>} scroll>
-                  <Navigation style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-                      <Link to="/aboutme"> About Me</Link>
-                  </Navigation>
-              </Header>
-              <Drawer title="Title">
-                  <Navigation>
-                      <Link to="/aboutme"> About Me</Link>
-                  </Navigation>
-              </Drawer>
-              <Content>
-                  <div className="page-content" />
-                  <Main/>
-              </Content>
-          </Layout>
-      </div>
-  );
+class App extends Component {
+
+    render() {
+        return (
+            <div className="demo-big-content">
+                <Layout>
+                    <Header className="header-color"
+                            title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Functionary</Link>}
+                            scroll>
+                        <Navigation
+                            style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+                            <Link to="/aboutme"> About Me</Link>
+                        </Navigation>
+                    </Header>
+                    <Drawer title="Title">
+                        <Navigation>
+                            <Link to="/aboutme"> About Me</Link>
+                        </Navigation>
+                        <Navigation>
+                            <Link to="/question"> Questions </Link>
+                        </Navigation>
+                    </Drawer>
+                    <Content>
+                        <div className="page-content"/>
+                        <Main/>
+                    </Content>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/main.js
+++ src/components/main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import LandingPage from './landingpage';
 import { Switch, Route } from 'react-router-dom';
 import About from './aboutme';
+import QuestionPage from './questions';
 
 const Main = () => (
     <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/aboutme" component={About} />
+        <Route path="/question" component={QuestionPage} />
     </Switch>
-)
+);
 
 export default Main;
