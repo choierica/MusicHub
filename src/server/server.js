4d8f88308f1e6b4f7ac1bdfac1b0bd952eb6686b
l1z1b <echoi@alumni.ubc.ca>
2019-07-06 22:50:20 -0700

    Process.env

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
 
