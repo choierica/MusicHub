a76a5c31d05e7030014595443bfd155fc8daa1c9
l1z1b <echoi@alumni.ubc.ca>
2019-07-10 12:52:13 -0700

    About page edit

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -10,7 +10,7 @@ class About extends Component {
                     <img src= {picture}  alt=""/>
                     <p> Hello everyone! My name is Erica, and I'm a fourth year undergraduate student at the University of
                         British Columbia. I am currently studying Computer Science, and I created MusicHub as a platform for people to share their favorite music..
-                        Feel free to listen to what I have available, and further share your own favorites!
+                        Feel free to listen to what I have shared on the main page, and please post your own favorites as well!
                     </p>
                 </div>
             </div>
